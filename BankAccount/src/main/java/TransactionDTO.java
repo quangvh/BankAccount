3331641b073fbe39fad5b8c69ceecbd5baf7d9a5
quangvh <vhquangbk307@gmail.com>
2013-06-12 15:35:15 +0700

    Create TransactionDTO.java

---
+++ BankAccount/src/main/java/TransactionDTO.java
@@ -0,0 +1,33 @@
+/**
+ * User: quangvh
+ * Date: 6/12/13
+ */
+public class TransactionDTO {
+    private String accountNumber;
+    private double amount;
+    private String description;
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
